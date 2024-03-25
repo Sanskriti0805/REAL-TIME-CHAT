3a4e5026c44c6191855ec44f0dce6910fcc2d125
Sanskriti Arora <Sanskritiarora2005@gmail.com>
2024-03-25 22:36:05 +0530

    user management

--- src/UserManager.ts
+++ src/UserManager.ts
@@ -9,25 +9,29 @@ interface Room {
 }
 
 export class UserManager {
-    private users : Map<string, Room>
+    private rooms : Map<string, Room>
     constructor() {
-        this.users = new Map<string, Room>
+        this.rooms = new Map<string, Room>
     }
 
     addUser(name: string, userid: string, roomId: string, socket: WebSocket) {
-        if (!this.users.get(roomId)) {
-            this.users.set(roomId,{
+        if (!this.rooms.get(roomId)) {
+            this.rooms.set(roomId,{
                 users: []
             })
               
         }
-        this.users.get(roomId)?.users.push({
+        this.rooms.get(roomId)?.users.push({
             id: userid,
             name,
         })
 
     }
     removeUser(roomId: string, userId: string) {
-
+        const users =  this.rooms.get(roomId)?.users;
+        if (users) {
+            users.filter(({id}) => id === userId);
+        }
+        
     }
 }
